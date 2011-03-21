c27b86822014c4a518a1f0adc2d8df4335c2e08d
Максим <gofl@meta.ua>
2011-03-21 17:14:00 +0200

    щоденне оновлення

--- src/Zayavku.java
+++ src/Zayavku.java
@@ -1,28 +1,32 @@
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Monitor;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Combo;
+
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Rectangle;
 
-import java.awt.Dimension;
-import java.awt.Toolkit;
-
+import java.io.InputStream;
 import java.sql.*;
 import java.util.*;
 
 import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.layout.FormAttachment;
-import org.eclipse.swt.widgets.Combo;
-import org.eclipse.wb.swt.SWTResourceManager;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
 
 public class Zayavku {
 
@@ -34,8 +38,7 @@ public class Zayavku {
 	private static Image trueImage;
 	private static Image falseImage;
 	private static Display display;
-	private Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-
+	
 	/**
 	 * Launch the application.
 	 * 
@@ -70,15 +73,29 @@ public class Zayavku {
 	 * Create contents of the window.
 	 */
 	protected void createContents() {
-		trueImage = new Image(display, "icons/OK.png");
-		falseImage = new Image(display, "icons/Denied.png");
+		try {
+		InputStream true_image = Zayavku.class.getClassLoader().getResourceAsStream("OK.png");
+		InputStream false_image = Zayavku.class.getClassLoader().getResourceAsStream("Denied.png");
+		//trueImage = new Image(display, new FileInputStream("./icon/OK.png"));
+		trueImage = new Image(display, true_image);
+		falseImage = new Image(display, false_image);
+		} catch (IllegalArgumentException e) {
+			System.out.println(e);
+		}
 
-		shell = new Shell();
-		shell.setSize(1304, 768);
-		shell.setLocation((screenSize.width - shell.getSize().x) / 2,
-				(screenSize.height - shell.getSize().y) / 2);
+		shell = new Shell(display);
+		shell.setSize(1304, 900);
 		shell.setText("Заявки");
 		shell.setLayout(new FormLayout());
+
+		Monitor primary = display.getPrimaryMonitor();
+		Rectangle bounds = primary.getBounds();
+		Rectangle rect = shell.getBounds();
+
+		int x = bounds.x + (bounds.width - rect.width) / 2;
+		int y = bounds.y + (bounds.height - rect.height) / 2;
+
+		shell.setLocation(x, y);
 
 		Menu menu = new Menu(shell, SWT.BAR);
 		shell.setMenuBar(menu);
@@ -98,8 +115,15 @@ public class Zayavku {
 		});
 		mntmExit_2.setText("Exit");
 
-		table = new Table(shell, SWT.BORDER | SWT.FULL_SELECTION);
-		table.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+		table = new Table(shell, SWT.BORDER | SWT.FULL_SELECTION | SWT.MULTI);
+		table.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDoubleClick(MouseEvent e) {
+				String string = "";
+				//TableItem [] selection = table.getSelection();
+				System.out.println(string);
+			}
+		});
 		FormData fd_table = new FormData();
 		fd_table.bottom = new FormAttachment(100, -10);
 		fd_table.left = new FormAttachment(0, 10);
@@ -109,7 +133,7 @@ public class Zayavku {
 		table.setLinesVisible(true);
 
 		TableColumn tblclmnNewColumn_1 = new TableColumn(table, SWT.LEFT);
-		tblclmnNewColumn_1.setWidth(120);
+		tblclmnNewColumn_1.setWidth(150);
 		tblclmnNewColumn_1.setText("Вулиця");
 
 		TableColumn tblclmnNewColumn = new TableColumn(table, SWT.LEFT);
@@ -133,48 +157,55 @@ public class Zayavku {
 		tableColumn_3.setText("Час Виклику");
 
 		TableColumn tableColumn_4 = new TableColumn(table, SWT.LEFT);
-		tableColumn_4.setWidth(100);
+		tableColumn_4.setWidth(54);
 		tableColumn_4.setText("Статус");
 
 		TableColumn tableColumn_5 = new TableColumn(table, SWT.LEFT);
-		tableColumn_5.setWidth(147);
-		tableColumn_5.setText("Домашній телефон");
+		tableColumn_5.setWidth(62);
+		tableColumn_5.setText("Д.тел.");
 
 		TableColumn tableColumn_6 = new TableColumn(table, SWT.LEFT);
-		tableColumn_6.setWidth(150);
-		tableColumn_6.setText("Мобільний телефон");
+		tableColumn_6.setWidth(100);
+		tableColumn_6.setText("Моб.тел.");
 
 		TableColumn tblclmnNewColumn_2 = new TableColumn(table, SWT.LEFT);
-		tblclmnNewColumn_2.setWidth(100);
+		tblclmnNewColumn_2.setWidth(150);
 		tblclmnNewColumn_2.setText("Виконавець");
 
 		TableColumn tableColumn_7 = new TableColumn(table, SWT.LEFT);
 		tableColumn_7.setWidth(110);
 		tableColumn_7.setText("Коментарі");
 
-		combo = new Combo(shell, SWT.NONE);
-		fd_table.top = new FormAttachment(combo, 6);
-		combo.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+		Group group = new Group(shell, SWT.NONE);
+		fd_table.top = new FormAttachment(group, 6);
+		group.setText("Статус");
+		group.setLayout(new FormLayout());
+		FormData fd_group = new FormData();
+		fd_group.left = new FormAttachment(0, 10);
+		group.setLayoutData(fd_group);
+
+		combo = new Combo(group, SWT.NONE);
+		fd_group.top = new FormAttachment(combo, 3);
+		FormData fd_combo = new FormData();
+		fd_combo.top = new FormAttachment(0);
+		fd_combo.left = new FormAttachment(0);
+		fd_combo.right = new FormAttachment(0, 183);
+		combo.setLayoutData(fd_combo);
 		combo.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				table.removeAll();
-				db_connect();
-				select_all(combo.getSelectionIndex());
+				refresh_grid(combo.getSelectionIndex());
 			}
 		});
 		combo.setItems(new String[] { "Всі", "Виконані", "Не виконані" });
-		FormData fd_combo = new FormData();
-		fd_combo.top = new FormAttachment(0, 10);
-		fd_combo.left = new FormAttachment(0, 10);
-		combo.setLayoutData(fd_combo);
 		combo.select(2);
-		shell.setTabList(new Control[]{table});
+		refresh_grid(2);
+		shell.setTabList(new Control[] { table });
 	}
 
 	private static void db_connect() {
 		try {
-			String url = "jdbc:postgresql://localhost/zayavka";
+			String url = "jdbc:postgresql://192.168.33.116/zayavka";
 			Properties props = new Properties();
 			props.setProperty("user", "postgres");
 			props.setProperty("password", "palamarc");
@@ -187,6 +218,7 @@ public class Zayavku {
 
 	private static void select_all(int status) {
 		String stat = new String();
+		Color red = new Color(display, 255, 204, 204);
 		switch (status) {
 		case 0:
 			stat = "";
@@ -202,18 +234,22 @@ public class Zayavku {
 		try {
 			Statement st = conn.createStatement();
 			ResultSet rs = st
-					.executeQuery("SELECT sorting, kv, login, zayavka_error.name, date, time ,status, domtel, mobtel, zayavka_worker.name, comments "
-							+ "FROM zayavka_zayavka, zayavka_dom, zayavka_error, zayavka_worker "
+					.executeQuery("SELECT sorting, kv, login, zayavka_error.name, date, zayavka_time_choices.descr ,status, domtel, mobtel, zayavka_worker.name, comments "
+							+ "FROM zayavka_zayavka, zayavka_dom, zayavka_error, zayavka_worker, zayavka_time_choices "
 							+ "WHERE zayavka_dom.id = zayavka_zayavka.vyl_id "
 							+ "AND zayavka_zayavka.error_id = zayavka_error.id "
 							+ "AND zayavka_zayavka.who_do_id = zayavka_worker.id "
+							+ "AND zayavka_time_choices.time_id = zayavka_zayavka.time "
 							+ stat);
 			while (rs.next()) {
 				TableItem tableItem = new TableItem(table, SWT.NONE);
+
 				tableItem.setText(0, rs.getString(1));
+				tableItem.setBackground(0, red);
 				tableItem.setText(1, rs.getString(2));
 				tableItem.setText(2, rs.getString(3));
 				tableItem.setText(3, rs.getString(4));
+
 				try {
 					tableItem.setText(4, rs.getDate(5).toString());
 				} catch (NullPointerException e) {
@@ -244,5 +280,11 @@ public class Zayavku {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+	}
+
+	private static void refresh_grid(int status) {
+		table.removeAll();
+		db_connect();
+		select_all(status);
 	}
 }
